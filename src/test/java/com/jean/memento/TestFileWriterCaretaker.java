154020e00cedf8ce03874850c7f1f48850abab6d
aljean <aljean@lubotin.com>
2015-05-09 13:58:54 +0300

    from Maven to Gradle

---
+++ src/test/java/com/jean/memento/TestFileWriterCaretaker.java
@@ -0,0 +1,29 @@
+package com.jean.memento;
+
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by jean on 09.05.15.
+ */
+public class TestFileWriterCaretaker {
+
+    @Test
+    public void methodTest(){
+        assertEquals("10", "10");
+    }
+}
+
+
+//import org.junit.*;
+//        import static org.junit.Assert.assertEquals;
+//
+//public class TestMain {
+//
+//    @Test
+//    public void getMsgTest(){
+//        Main main = new Main();
+//        String str = main.getMsg();
+//        assertEquals(str , "2 b || not to 2");
+//    }
+//}
